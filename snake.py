0a3f64e229253702ba3a9dd59bde18682b580bfe
APr0Guy <himanshusah2000@gmail.com>
2026-05-12 20:15:29 +0545

    idk

--- snake.py
+++ snake.py
@@ -10,12 +10,17 @@ class snake:
     def bg_make(self):
         x = y = 0 # 40x45 (y,x) x is 15 times y is 12 times and diff btwn 2 x is 15
         for i in range(180):
-            btn = tk.Button(text=i,width=5,height=2).place(x=x,y=y)
-            x+=45
+            btn = tk.Button(text=i,width=5,height=2,bg='black',fg='black') ; btn.pack()
+            btn.place(x=x,y=y)
+            if x <= 15*45:
+                x += 45
+                if x == 15*45:
+                    x = 0
+                    y += 40
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.geometry('720x560')
+    root.geometry('675x480')
     root.title('Snake')
     root.resizable(False, False)
     snake(root)
